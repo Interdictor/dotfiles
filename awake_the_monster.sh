d1d1870319f2b7067d5e69a7d55fca2c868509c0
Interdictor <interdictor.dsg@gmail.com>
2017-03-21 07:42:33 +0100

    Add awake the monster script

---
+++ awake_the_monster.sh
@@ -0,0 +1,27 @@
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo add-apt-repository 'ppa:webupd8team/sublime-text-3'
+sudo add-apt-repository 'ppa:webupd8team/atom'
+sudo add-apt-repository ppa:kritalime/ppa
+
+sudo apt-get update
+sudo apt -y install unity-tweak-tool
+sudo apt -y install gdebi
+sudo apt -y install git
+sudo apt -y install vim
+sudo apt -y install 'ruby2.4 ruby2.4-dev'
+
+sudo apt -y install krita
+
+sudo apt -y install build-essentials
+sudo apt -y install -y libssl-dev libreadline-dev zlib1g-dev
+sudo apt-get -y 'install libxss1 libappindicator1 libindicator7'
+
+wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+sudo gdebi -iy google-chrome*.deb
+
+sudo apt-get -y install 'sublime-text-installer'
+sudo apt-get -y install 'atom'
+
+sudo rm google-chrome*.deb
+
+
