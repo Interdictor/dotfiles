c16928829d89ef25da59eaf95fce81cdce15dce4
interdictor <interdictor.dsg@gmail.com>
2019-01-21 19:02:58 +0100

    Create environment variables file instead of exporting directly

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+echo 'export LANG=C.UTF-8' >> /etc/environment/python.sh
+chmod +x /etc/environment/python.sh
 
 apt-get update
 
 apt-get install -y python3 python3-pip git curl
 pip3 install pipenv
+pipenv --version
