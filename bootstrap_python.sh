4ac9fe7ed91b9e410a76c4d017a4d3d83af31ca6
interdictor <interdictor.dsg@gmail.com>
2019-01-21 19:32:35 +0100

    Remove environment vars settings

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'export LANG=C.UTF-8' >> /etc/environment/python.sh
-chmod +x /etc/environment/python.sh
-
 apt-get update
 
 apt-get install -y python3 python3-pip git curl
