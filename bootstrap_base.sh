3536d234d7312ba8eb5b6b901151eda54abbb437
Leo Rejón <Interdictor.dsg@gmail.com>
2020-07-29 15:54:56 +0200

    Update bootstrap_base.sh

--- bootstrap_base.sh
+++ bootstrap_base.sh
@@ -1,13 +1,7 @@
 sudo apt-get -y update
-sudo apt -y install git vim gdebi curl
-curl -sSL https://get.docker.com/ | sh
+sudo apt -y install git vim curl
 
 git config --global core.editor vim
 git config --global push.default simple
 git config --global rerere.enabled true
 git config --global pull.rebase true
-
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
